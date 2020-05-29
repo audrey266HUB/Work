d46b75ea9a70156486d63823ecff46c8c08dfcce
greg-Da <gregoiredalmasso@gmail.com>
2020-05-29 21:25:13 +0200

    auth

--- src/app/firebase.ts
+++
@@ -1,9 +0,0 @@
-export const config = { 
-    apiKey: "AIzaSyBbS08gpr1Wd4Xo9V8sffataEX4uJXvZF4",
-    authDomain: "work-c18db.firebaseapp.com",
-    databaseURL: "https://work-c18db.firebaseio.com",
-    projectId: "work-c18db",
-    storageBucket: "work-c18db.appspot.com",
-    messagingSenderId: "74732171020",
-    appId: "1:74732171020:web:96a290d8556a741fc5e329"
-  }
